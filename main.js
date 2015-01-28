1e29dcfc38c177d856574c7f59cb69fef001c1ea
Michael Germany <mike@rickgermany.com>
2015-01-28 12:38:42 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+//Function Practice #2
+
+
+
+//1.
+
+// var myObj = {
+// 	name: 'Luisa',
+// 	age: 25
+// };
+
+// var getName = function(key) {
+// 	console.log(key.name);
+// };
+
+// getName(myObj);
+
+
+
+//2.
+
+
+var myArray = ['javascript', 'is', 'awesome'];
+var anotherArray = ['what', 'happened', 'to', 'my', 'function'];
+
+var totalLetters = function(arg) {
+	console.log(arg.split(""));
+};
+
+totalLetters(myArray);
